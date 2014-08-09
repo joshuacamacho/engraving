a3a3de16a41737d60473534ce58d3d642bb03f15
Josh <joshuajcamacho@gmail.com>
2014-08-08 23:03:10 -0700

    0.04

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-echo "test";
+	//log the user in if login form was used
+	if(isset($_POST['email']) && isset($_POST['password'])){
+			require_once("connect.php");
+			$email=mysqli_real_escape_string($link,$_POST['email']);
+			$password=mysqli_real_escape_string($link,$_POST['password']);
+			$query="SELECT password FROM users WHERE email='".$email."'";
+			$result = $link->query($query);
+			while($row=mysqli_fetch_array($result)){
+				if($password == $row['password']){
+					echo "LOGIN COMPLETE";
+				}
+			}
+	}
+
+
+
+	echo "
+				login
+				<form action='./' method='post'>
+				<input type='text' name='email'>
+				<input type='password' name='password'>
+				<input type='submit'>
+
+	";
+
+	echo "<a href='./register.php'>Register</a>"
+
 ?>
---
+++ register.php
@@ -0,0 +1,56 @@
+<?php
+	if(
+		isset($_POST['firstname']) &&
+		isset($_POST['lastname']) &&
+		isset($_POST['email']) &&
+		isset($_POST['password'])
+		){
+			require_once("connect.php");
+			$firstname=mysqli_real_escape_string($link,$_POST['firstname']);
+			$lastname=mysqli_real_escape_string($link,$_POST['lastname']);
+			$email=mysqli_real_escape_string($link,$_POST['email']);
+			$password=mysqli_real_escape_string($link,$_POST['password']);
+
+			
+			if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+				//IF EMAIL is valid form
+				echo "validemailform";
+				$query="SELECT email FROM users WHERE email='".$email."'";
+				$result=$link->query($query);
+				$row=$result->fetch_array();
+				echo "<pre>";
+				print_r($row);
+				echo "</pre>";
+
+				if(count($row)>0){
+					//if email exists
+					echo "email exists";
+				}
+				else{
+					//if email is unique and valid form
+					 echo "email unique";
+				}
+
+
+
+			}else{
+				//if email is invalid form
+				echo "invalid emaIL FORM";
+
+			}
+			
+	}
+
+	echo "<form action='register.php' method='post'>
+				<label>First Name</label>
+				<input type='text' name='firstname'>
+				<label>Last Name</label>
+				<input type='text' name='lastname'>
+				<label>Email</label>
+				<input type='text' name='email'>
+				<label>Password</label>
+				<input type='password' name='password'>
+				<input type='submit'>
+	";
+
+?>
