789c2dd20fbaf6f559a86732709cc3e3e627dff4
joshuacamacho <joshuajcamacho@gmail.com>
2014-09-19 15:36:47 -0700

    stuffs

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
           <ul>
             <li><a href="#"><img src="images/slideshow.jpg" alt="" /></a></li>
             <li><a href="#"><img src="images/slideshow2.jpg" alt="" /></a></li>
-            <li><a href="#"><img src="images/slideshow3.jpg" alt="" /></a></li>
-            <li><a href="#"><img src="images/slideshow4.jpg" alt="" /></a></li>
+            <!--<li><a href="#"><img src="images/slideshow3.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="images/slideshow4.jpg" alt="" /></a></li>-->
           </ul>
         </div>
         <div id="slider-nav"> <a href="#" class="active">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> </div>
@@ -20,19 +20,31 @@
  <div id="content">
     
    	  <div id="content_left">
-        	<h1>Welcome to Timeless Pieces Watch Engraving</h1>
+        	<h1>Welcome to Rock Solid Business Cards</h1>
           <p>Aliquam tristique lacus in sapien. Suspendisse potenti. Ut sed pede. Nullam vitae tellus. Sed ultrices. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc quis sem nec tellus blandit tincidunt. Aliquam tristique lacus in sapien. Suspendisse potenti. Ut sed pede. Nullam vitae tellus. Sed ultrices. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
                <!-- makes layout space -->          
              <div class="spacer_with_height">&nbsp;</div>
              
              <h2 id="label">New Products</h2>
-             <div class="product_box">
-             	<h3 class="product_name">Men's Silver Watch</h3>
-                <img src="images/silvermen.jpg" alt="image" />
-                <p>Aliquam tristique lacus in sapien. Suspendisse potenti.</p>
-                <div class="price">PRICE:<span>$90.00</span></div>                           
-             	<div class="buynow"><a href="shoppingcard.html">Buy Now</a></div><a href="#">Details</a>
-             </div>
+
+             <?php 
+                $query=("SELECT * FROM items WHERE stocklevel='In Stock'");
+                $result=$link->query($query);
+                while($row=mysqli_fetch_assoc($result)){
+                    echo "<div class='product_box'>
+                        <h3 class='product_name'>".$row['name']."</h3>
+                        <a href='catalog.php?itemid=".$row['itemid']."'>
+                            <img src='img/".$row['pictureurl']."' width='200px' alt='image' />
+                        </a>
+                        <p>".$row['description']."</p>
+                        <div class='price'>PRICE:<span>$90.00</span></div>                           
+                        <div class='buynow'><a href='catalog.php?itemid=".$row['itemid']."'>Buy Now</a></div>
+                        </div>";
+                }
+             ?>
+
+
+             <!--
              <div class="product_box">
              	<h3 class="product_name">Men's Gold Watch</h3>
                 <img src="images/goldmen.jpg" alt="image" />
@@ -46,9 +58,10 @@
                 <p>Suspendisse potenti. Ut sed pede. Nullam vitae tellus. </p>
                 <div class="price">PRICE:<span>$80.00</span></div>                           
              	<div class="buynow"><a href="shoppingcard.html">Buy Now</a></div><a href="#">Details</a>
-             </div>
-             
-			<div class="spacer_with_height">&nbsp;</div> <!-- makes layout space --> 
+             </div>-->
+             <!--
+			<div class="spacer_with_height">&nbsp;</div>--> <!-- makes layout space --> 
+            <!--
             <div class="product_box">
              	<h3 class="product_name">Women's Gold Watch</h3>
                 <img src="images/goldwomen.jpg" alt="image" />
@@ -62,7 +75,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing.</p>
                 <div class="price">PRICE:<span>$190.00</span></div>                           
              	<div class="buynow"><a href="shoppingcart.html">Buy Now</a></div><a href="#">Details</a>
-           </div>
+           </div>-->
     	</div> 
     	<!-- end of ocntent left -->
         
